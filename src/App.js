c4b5ac3a17b5dce9c52b003753c49d0445f19016
Michael Sutcliffe <m.sutcliffe2303@icloud.com>
2024-02-17 15:45:47 +0000

    Testing importing other files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,1 @@
+<header><h1>Hello World!</h1></header>
